315daca457230cd21b09b69a09b52173d94f9c11
iiroalhonen <iiro.alhonen@kpsbremen.de>
2021-07-10 20:12:20 +0200

    Renamed files for easier use, version 0.2.0

--- Package.swift
+++ Package.swift
@@ -13,7 +13,7 @@ let package = Package(
         // Products define the executables and libraries a package produces, and make them visible to other packages.
         .library(
             name: "Nifty Markdown Formatter",
-            targets: ["Nifty Markdown Formatter"]),
+            targets: ["NiftyMarkdownFormatter"]),
     ],
     dependencies: [
         // Dependencies declare other packages that this package depends on.
@@ -23,10 +23,10 @@ let package = Package(
         // Targets are the basic building blocks of a package. A target can define a module or a test suite.
         // Targets can depend on other targets in this package, and on products in packages this package depends on.
         .target(
-            name: "Nifty Markdown Formatter",
+            name: "NiftyMarkdownFormatter",
             dependencies: []),
         .testTarget(
-            name: "Nifty Markdown FormatterTests",
-            dependencies: ["Nifty Markdown Formatter"]),
+            name: "NiftyMarkdownFormatterTests",
+            dependencies: ["NiftyMarkdownFormatter"]),
     ]
 )
